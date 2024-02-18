973a5a9be9f24d9a28dacad0568b3cda2a4e7eb6
cjia86 <c_jia@icloud.com>
2024-02-18 15:25:14 -0800

    install in non-interactive mode

--- docs/prepare_env/setup_sagemaker.sh
+++ docs/prepare_env/setup_sagemaker.sh
@@ -2,13 +2,13 @@
 
 conda create -n real3dportrait python=3.9
 conda activate real3dportrait
-conda install ipykernel
-conda install conda-forge::ffmpeg
-conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch-nightly -c nvidia
-conda install pytorch3d::pytorch3d
+conda install ipykernel -y
+conda install conda-forge::ffmpeg -y
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch-nightly -c nvidia -y
+conda install pytorch3d::pytorch3d -y
 
-pip install cython
-pip install openmim==0.3.9
-mim install mmcv==2.1.0 # use mim to speed up installation for mmcv
+pip install --yes cython
+pip install --yes openmim==0.3.9
+mim install --yes mmcv==2.1.0 # use mim to speed up installation for mmcv
 
-pip install -r /home/ec2-user/SageMaker/Real3DPortrait/docs/prepare_env/requirements.txt -v
+pip install --yes -r /home/ec2-user/SageMaker/Real3DPortrait/docs/prepare_env/requirements.txt -v
